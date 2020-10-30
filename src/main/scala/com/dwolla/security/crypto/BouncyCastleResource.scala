5c894479670e7a75502df838ddb263113763fcc2
Brian P. Holt <bholt@dwolla.com>
2020-10-30 15:50:18 -0500

    track whether BouncyCastle was already registered, and only unregister on shutdown if we were the ones who added it

--- src/main/scala/com/dwolla/security/crypto/BouncyCastleResource.scala
+++ src/main/scala/com/dwolla/security/crypto/BouncyCastleResource.scala
@@ -10,17 +10,28 @@ sealed trait BouncyCastleResource
 object BouncyCastleResource {
   def apply[F[_] : Sync : ContextShift](blocker: Blocker,
                                         removeOnClose: Boolean = true): Resource[F, BouncyCastleResource] = {
-    def registerBouncyCastle: F[String] =
+    def registerBouncyCastle: F[BouncyCastleRegistrationToken] =
       for {
         provider <- blocker.delay(new BouncyCastleProvider)
-        _ <- blocker.delay(Security.addProvider(provider))
-      } yield provider.getName
+        pos <- blocker.delay(Security.addProvider(provider))
+      } yield BouncyCastleRegistrationToken(pos, provider.getName)
 
-    def removeBouncyCastle(name: String): F[Unit] =
-      if (removeOnClose)
-        blocker.delay(Security.removeProvider(name))
-      else ().pure[F]
+    def removeBouncyCastle(token: BouncyCastleRegistrationToken): F[Unit] =
+      token match {
+        case Registered(name) if removeOnClose => blocker.delay(Security.removeProvider(name))
+        case _ => ().pure[F]
+      }
 
     Resource.make(registerBouncyCastle)(removeBouncyCastle).as(new BouncyCastleResource {})
   }
 }
+
+private[crypto] sealed trait BouncyCastleRegistrationToken
+private[crypto] object BouncyCastleRegistrationToken {
+  def apply(position: Int, name: String): BouncyCastleRegistrationToken =
+    if (-1 == position) AlreadyRegistered
+    else Registered(name)
+}
+
+private[crypto] case class Registered(name: String) extends BouncyCastleRegistrationToken
+private[crypto] case object AlreadyRegistered extends BouncyCastleRegistrationToken
