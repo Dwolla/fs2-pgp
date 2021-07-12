20d81a28b05c6135f80648df562fbd7233031aaf
Brian Holt <bholt@dwolla.com>
2021-07-12 13:19:41 -0500

    use 1 minute timeout for all ScalaCheck effectful tests

--- tests/src/test/scala/com/dwolla/security/crypto/Fs2PgpSpec.scala
+++ tests/src/test/scala/com/dwolla/security/crypto/Fs2PgpSpec.scala
@@ -1,9 +1,8 @@
 package com.dwolla.security.crypto
 
-import cats.effect.{Effect, IO, Resource}
+import cats.effect._
 import cats.effect.testing.scalatest.AsyncIOSpec
-import cats.effect.testing.scalatest.scalacheck.EffectCheckerAsserting
-import com.dwolla.testutils.{PgpArbitraries, ResourceCheckerAsserting}
+import com.dwolla.testutils.{PgpArbitraries, ResourceCheckerAsserting, TimeoutEffectCheckerAsserting}
 import org.scalatest.AsyncTestSuite
 import org.scalatest.matchers.should.Matchers
 import org.scalatestplus.scalacheck.{CheckerAsserting, ScalaCheckPropertyChecks}
@@ -16,6 +15,6 @@ trait Fs2PgpSpec
   protected implicit def ioCheckingAsserting[A]: CheckerAsserting[Resource[IO, A]] { type Result = IO[Unit] } =
     new ResourceCheckerAsserting[IO, A]
 
-  protected implicit def effectCheckingAsserting[F[_] : Effect, A]: CheckerAsserting[F[A]] { type Result = F[Unit] } =
-    new EffectCheckerAsserting[F, A]
+  protected implicit def timeoutEffectCheckerAsserting[F[_] : ConcurrentEffect : Timer, A]: CheckerAsserting[F[A]] { type Result = F[Unit] } =
+    new TimeoutEffectCheckerAsserting[F, A]
 }
