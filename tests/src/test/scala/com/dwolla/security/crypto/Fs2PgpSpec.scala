d94daeb9b9248a6070a3b722c00f894ef450ac12
Brian Holt <bholt@dwolla.com>
2021-07-22 01:36:14 -0500

    add a 2 minute timeout to all tests

--- tests/src/test/scala/com/dwolla/security/crypto/Fs2PgpSpec.scala
+++ tests/src/test/scala/com/dwolla/security/crypto/Fs2PgpSpec.scala
@@ -4,14 +4,20 @@ import cats.effect._
 import cats.effect.testing.scalatest.AsyncIOSpec
 import com.dwolla.testutils.{PgpArbitraries, ResourceCheckerAsserting, TimeoutEffectCheckerAsserting}
 import org.scalatest.AsyncTestSuite
+import org.scalatest.concurrent.AsyncTimeLimitedTests
 import org.scalatest.matchers.should.Matchers
+import org.scalatest.time._
 import org.scalatestplus.scalacheck.{CheckerAsserting, ScalaCheckPropertyChecks}
 
 trait Fs2PgpSpec
   extends AsyncIOSpec
     with Matchers
     with ScalaCheckPropertyChecks
+    with AsyncTimeLimitedTests
     with PgpArbitraries { asyncTestSuite: AsyncTestSuite =>
+
+  override def timeLimit: Span = Span(2, Minutes)
+
   protected implicit def ioCheckingAsserting[A]: CheckerAsserting[Resource[IO, A]] { type Result = IO[Unit] } =
     new ResourceCheckerAsserting[IO, A]
 
